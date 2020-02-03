14fdc2440a0fac4198b98fd1578ff37fda67462d
xpress-embedo <laboratory.embedded@gmail.com>
2020-02-03 22:05:17 +0100

    * Python Program for Publishing and Subscribing using MQTT

---
+++ 02-mqtt_paho_sub_pub.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+
+# Callback Function on Connection with MQTT Server
+def on_connect( client, userdata, flags, rc):
+    print ("Connected with Code :" +str(rc))
+    # Subscribe Topic from here
+    client.subscribe("Test/#")
+
+# Callback Function on Receiving the Subscribed Topic/Message
+def on_message( client, userdata, msg):
+    # print the message received from the subscribed topic
+    print ( str(msg.payload) )
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("m14.cloudmqtt.com", 18410, 60)
+client.username_pw_set("setsmjwc", "apDnKqHRgAjA")
+
+client.loop_forever()
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Python
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.me/embeddedlab)
+
+# Python Program Examples
+
+### [01-mqtt_paho_sub.py](https://embeddedlaboratory.blogspot.com/2018/01/getting-started-with-mqtt-using.html)
+MQTT Subscription Example using Python
+### [02-mqtt_paho_sub_pub.py](https://embeddedlaboratory.blogspot.com/2018/01/getting-started-with-mqtt-using.html)
+MQTT Subscription and Publish Example using Python
